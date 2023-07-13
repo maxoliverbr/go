f54d5b66a4f3c2d6c42caedeea8a9d58a681cef7
maxoliverbr <max.oliver@cintrax.com.br>
2023-07-12 21:30:24 -0300

    Exercise completed

--- exercises/concept/airport-robot/airport_robot.go
+++ exercises/concept/airport-robot/airport_robot.go
@@ -1,5 +1,49 @@
 package airportrobot
 
+type Greeter interface {
+	LanguageName() string
+	Greet(g string) string
+}
+
+type German struct {
+	Language string
+}
+
+type Italian struct {
+	Language string
+}
+
+type Portuguese struct {
+	Language string
+}
+
+func (German) LanguageName() string {
+	return "German"
+}
+
+func (German) Greet(name string) string {
+	return "Hallo " + name + "!"
+}
+
+func (Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (Italian) Greet(name string) string {
+	return "I can speak Italian: Ciao " + name + "!"
+}
+func (Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (Portuguese) Greet(name string) string {
+	return "I can speak Portuguese: Olá " + name + "!"
+}
+
+func SayHello(name string, g Greeter) string {
+	return g.Greet(name)
+}
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
