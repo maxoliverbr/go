68371ef2f9d849f0104cc491630a074ddcaee85f
maxoliverbr <max.oliver@cintrax.com.br>
2023-07-15 13:14:54 -0300

    Exercise completed

--- exercises/practice/collatz-conjecture/collatz_conjecture.go
+++ exercises/practice/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,23 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("true")
+	} else if n == 1 {
+		return 0, nil
+	}
+	i := 1
+	for {
+		if n%2 == 0 {
+			n = n / 2
+		} else if n != 1 {
+			n = 3*n + 1
+		} else if n == 1 {
+			break
+		}
+		i++
+	}
+	return i - 1, nil
 }
