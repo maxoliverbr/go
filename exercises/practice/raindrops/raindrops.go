e6cc14b0d5bcaeb60b921fbed0f7dea4a16fdabf
maxoliverbr <max.oliver@cintrax.com.br>
2023-07-15 12:40:47 -0300

    Exercise completed

--- exercises/practice/raindrops/raindrops.go
+++ exercises/practice/raindrops/raindrops.go
@@ -1,5 +1,20 @@
 package raindrops
 
+import "strconv"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	o := ""
+	if number%3 == 0 {
+		o = "Pling"
+	}
+	if number%5 == 0 {
+		o += "Plang"
+	}
+	if number%7 == 0 {
+		o += "Plong"
+	}
+	if o != "" {
+		return o
+	}
+	return strconv.Itoa(number)
 }
