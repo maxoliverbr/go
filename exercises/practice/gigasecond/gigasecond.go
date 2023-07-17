f4129c2f288e169def08d8b65831858ff512c5f7
maxoliverbr <max.oliver@cintrax.com.br>
2023-07-16 21:20:19 -0300

    Exercise completed

--- exercises/practice/gigasecond/gigasecond.go
+++ exercises/practice/gigasecond/gigasecond.go
@@ -10,9 +10,7 @@ import "time"
 
 // AddGigasecond should have a comment documenting it.
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	gigaSecond := 1000000000
+	t = t.Add(time.Second * time.Duration(gigaSecond))
 	return t
 }
